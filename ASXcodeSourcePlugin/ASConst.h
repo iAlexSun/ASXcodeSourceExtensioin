d1e12d5451f51573e41a1cb8da0e00a910894411
Sun Wen <wensun025217@sohu-inc.com>
2019-10-15 15:49:50 +0800

    修改通用规则可以通过ruleplist自动生成对应格式

--- ASXcodeSourcePlugin/ASConst.h
+++ ASXcodeSourcePlugin/ASConst.h
@@ -8,68 +8,38 @@
 
 
 /*
- * 需要在如下标记下自动插入代码,可以根据业务需求自行更改标记内容。可以插入单个标记也可以插入多个标记同时自动生成代码
- *
- * kGetterFormater     设置属性的懒加载方法标记格式
- * kAddSubviewFormater 设置UI控件添加
- * kMasonryFormater    设置UI控件设置布局
- * kInitFormater       设置UI控件初始化方法
-*/
+// * 需要在如下标记下自动插入代码,可以根据业务需求自行更改标记内容。可以插入单个标记也可以插入多个标记同时自动生成代码
+// *
+// * kGetterFormater     设置属性的懒加载方法标记格式
+// * kAddSubviewFormater 设置UI控件添加
+// * kMasonryFormater    设置UI控件设置布局
+// * kInitFormater       设置UI控件初始化方法
+//*/
 static NSString *const kGetterFormater = @"#pragma mark - Getter";
-static NSString *const kAddSubviewFormater = @"- (void)addSubviews" ;
-static NSString *const kMasonryFormater = @"- (void)addConstraints";
-static NSString *const kInitFormater = @"[self bindViewModel]";
-static NSString *const kInitNodeFormater = @"//initSubNode"; 
-/*************************************************************************/
+//
+///*************************************************************************/
+//
+//static NSString *const kCommand = @"Command";
+//
+//static NSString *const kViewModel = @"ViewModel";
+//
+//
+///*************************************************************************/
+////自定义内容格式
+//static NSString *const kASButtonFormater = @"- (%@ *)%@{\n    if (_%@ == nil) {\n        _%@ = [[%@ alloc] init];\n        [_%@ setTitle:<#(nullable NSString *)#> forState:UIControlStateNormal];\n        [_%@ setImage:[UIImage imageNamed:<#(nullable NSString *)#>] forState:UIControlStateNormal];\n        [_%@ setTitleColor:<#(nullable UIColor *)#> forState:UIControlStateNormal];\n        _%@.font =  <#UIFONT_IOS_DEFAULT_6#>;\n        _%@.layer.cornerRadius = <#EVO_PIX_5#>;\n        _%@.layer.masksToBounds = YES;\n    }\n    return _%@;\n}";
+//
+//static NSString *const kASUILabelFormater = @"- (%@ *)%@{\n    if (_%@ == nil) {\n        _%@ = [[%@ alloc] init];\n        _%@.text = <#(nullable NSString *)#>;\n        _%@.font = <#UIFONT_IOS_DEFAULT_6#>;\n        _%@.textColor = <#UICOLOR_SOHU_PHE_G5#>;\n        _%@.textAlignment = <#NSTextAlignmentLeft#>;\n   }\n    return _%@;\n}";
+//
+//static NSString *const kASMasonryFormater = @"    [self.%@ mas_makeConstraints:^(MASConstraintMaker *make) {\n        make.top.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.bottom.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.left.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.right.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.height.mas_equalTo(<#EVO_PIX_4#>);\n        make.width.mas_equalTo(<#EVO_PIX_4#>);\n    }];";
+//
+//static NSString *const kASCommandFormater = @"-(RACCommand *)%@{  \n   if (!_%@) {\n        @weakify(self);\n         _%@ = [[RACCommand alloc]initWithSignalBlock:^RACSignal *(id x) {\n            @strongify(self);\n            return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {\n                [subscriber sendNext:x];\n                [subscriber sendCompleted];\n                return nil;\n            }];\n        }];\n    }\n    return _%@;\n}";
 
-static NSString *const kCommand = @"Command";
-static NSString *const kButton =  @"Button";
-static NSString *const kUIButton =  @"UIButton";
-static NSString *const kUILabel =  @"UILabel";
-static NSString *const kView = @"View";
-static NSString *const kViewModel = @"ViewModel";
-static NSString *const kLabel = @"Label";
-static NSString *const kUIImageView = @"UIImageView";
-static NSString *const kTextField = @"TextField";
-static NSString *const kTextView = @"TextView";
-
-//ASDK Node生成标记
-static NSString *const kDisplayNode = @"DisplayNode";
-static NSString *const kButtonNode = @"ButtonNode";
-static NSString *const kImageNode = @"ImageNode";
-static NSString *const kUINetworkImageNode = @"NetworkImageNode";
-static NSString *const kTextNode2 = @"TextNode2";
-static NSString *const kTextNode = @"TextNode";
-
-/*************************************************************************/
-//自定义内容格式
-static NSString *const kASButtonFormater = @"- (%@ *)%@{\n    if (_%@ == nil) {\n        _%@ = [[%@ alloc] init];\n        [_%@ setTitle:<#(nullable NSString *)#> forState:UIControlStateNormal];\n        [_%@ setImage:[UIImage imageNamed:<#(nullable NSString *)#>] forState:UIControlStateNormal];\n        [_%@ setTitleColor:<#(nullable UIColor *)#> forState:UIControlStateNormal];\n        _%@.font =  <#UIFONT_IOS_DEFAULT_6#>;\n        _%@.layer.cornerRadius = <#EVO_PIX_5#>;\n        _%@.layer.masksToBounds = YES;\n    }\n    return _%@;\n}";
-
-static NSString *const kASUILabelFormater = @"- (%@ *)%@{\n    if (_%@ == nil) {\n        _%@ = [[%@ alloc] init];\n        _%@.text = <#(nullable NSString *)#>;\n        _%@.font = <#UIFONT_IOS_DEFAULT_6#>;\n        _%@.textColor = <#UICOLOR_SOHU_PHE_G5#>;\n        _%@.textAlignment = <#NSTextAlignmentLeft#>;\n   }\n    return _%@;\n}";
-
-static NSString *const kASMasonryFormater = @"    [self.%@ mas_makeConstraints:^(MASConstraintMaker *make) {\n        make.top.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.bottom.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.left.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.right.equalTo(self.<#UIView#>).offset(<#EVO_PIX_4#>);\n        make.height.mas_equalTo(<#EVO_PIX_4#>);\n        make.width.mas_equalTo(<#EVO_PIX_4#>);\n    }];";
-
-static NSString *const kASCommandFormater = @"-(RACCommand *)%@{  \n   if (!_%@) {\n        @weakify(self);\n         _%@ = [[RACCommand alloc]initWithSignalBlock:^RACSignal *(id x) {\n            @strongify(self);\n            return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {\n                [subscriber sendNext:x];\n                [subscriber sendCompleted];\n                return nil;\n            }];\n        }];\n    }\n    return _%@;\n}";
-
-static NSString *const kASAddsubviewFormater = @"    [<#UIView#> addSubview:self.%@];";
+//static NSString *const kASAddsubviewFormater = @"    [<#UIView#> addSubview:self.%@];";
 
 static NSString *const kASCommonFormater = @"- (%@ *)%@{\n    if (_%@ == nil) {\n        _%@ = [[%@ alloc] init];\n    }\n    return _%@;\n}";
 
-static NSString *const kLabelInitFormater = @"        self.%@.text = <#(nullable NSString *)#>;";
-
-static NSString *const kUIButtonInitFormater = @"        [self.%@ setTitle:<#(nullable NSString *)#> forState:UIControlStateNormal];\n        [self.%@ setImage:[UIImage imageNamed:<#(nullable NSString *)#>] forState:UIControlStateNormal];";
-
-static NSString *const kUIImageViewInitFormater = @"        [self.%@ sd_setImageWithURL:[NSURL URLWithString:<#(nullable NSString *)#>] placeholderImage:[UIImage imageNamed:<#(nullable NSString *)#>]];";
-
-
-//ASDK初始化格式
-
-static NSString *const kUIDisplayNodeInitFormater = @"        self.%@ = [[ASDisplayNode alloc] init];\n        self.%@.backgroundColor = UIColor.<#shm_lightGray_1#>;\n        self.%@.layerBacked = YES;";
-
-static NSString *const kUIButtonNodeInitFormater = @"        self.%@ = [[ASButtonNode alloc] init];\n        [self.%@ setImage:<#(nullable UIImage *)#> forState:<#(UIControlState)#>];\n        [self.%@ addTarget:<#(nullable id)#> action:<#(nonnull SEL)#> forControlEvents:ASControlNodeEventTouchUpInside];\n        self.%@.layerBacked = YES;";
-
-static NSString *const kUIImageNodeInitFormater = @"        self.%@ = [[ASImageNode alloc] init];\n        [self.%@ addTarget:<#target#> action:@selector(<#selector#>) forControlEvents:ASControlNodeEventTouchUpInside];\n        self.%@.contentMode = <#UIViewContentMode#>;\n        self.%@.image = [UIImage imageNamed:<#imageName#>];\n        self.%@.layerBacked = YES;";
-
-static NSString *const kUINetworkImageNodeInitFormater = @"        self.%@ = [[ASNetworkImageNode alloc] init];\n        self.%@.URL = viewModel.<#URL#>;\n        self.%@.layerBacked = YES;";
-
-static NSString *const kUITextNode2InitFormater = @"        self.%@ = [[ASTextNode2 alloc] init];\n        self.%@.attributedText = viewModel.<#attributedText#>;\n        self.%@.maximumNumberOfLines = <#linecount#>;\n        self.%@.layerBacked = YES;";
+//static NSString *const kLabelInitFormater = @"UI";
+//
+//static NSString *const kUIButtonInitFormater = @"        [self.%@ setTitle:<#(nullable NSString *)#> forState:UIControlStateNormal];\n        [self.%@ setImage:[UIImage imageNamed:<#(nullable NSString *)#>] forState:UIControlStateNormal];";
+//
+//static NSString *const kUIImageViewInitFormater = @"        [self.%@ sd_setImageWithURL:[NSURL URLWithString:<#(nullable NSString *)#>] placeholderImage:[UIImage imageNamed:<#(nullable NSString *)#>]];";
--- ASXcodeSourcePlugin/AutoLayout/ASAutoLayoutViewCode.m
+++ ASXcodeSourcePlugin/AutoLayout/ASAutoLayoutViewCode.m
@@ -12,13 +12,10 @@
 
 @interface ASAutoLayoutViewCode()
 
-@property(nonatomic,copy)NSMutableArray *lazyArray;
+@property(nonatomic,strong)NSMutableArray *lazyArray;
 
-@property(nonatomic,copy)NSMutableArray *containtsArray;
+@property(nonatomic,strong)NSMutableDictionary *propertyDics;
 
-@property(nonatomic,copy)NSMutableArray *subviewsArray;
-
-@property(nonatomic,copy)NSMutableArray *propertyValueArray;
 //字符流的行数
 @property(nonatomic,assign)NSUInteger lineCount;
 
@@ -29,14 +26,13 @@
 - (void)addAutoLayoutViewCodeWithInvocation:(XCSourceEditorCommandInvocation *)invocation {
     for (XCSourceTextRange *rang in invocation.buffer.selections) {
         [self initWithFormaterArray:rang invocation:invocation];
+        //替换字符串
+        [self addBufferInsertInvocation:invocation];
     }
 }
 -(void)initWithFormaterArray:(XCSourceTextRange *)rang invocation:(XCSourceEditorCommandInvocation *)invocation {
     [self.lazyArray removeAllObjects];
-    [self.containtsArray removeAllObjects];
-    [self.propertyValueArray removeAllObjects];
-    [self.subviewsArray removeAllObjects];
-    
+   
     NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"rules" ofType:@"plist"];
     NSMutableDictionary *dataDic = [[NSMutableDictionary alloc]initWithContentsOfFile:plistPath];
         
@@ -67,58 +63,34 @@
             classNameStr = [string stringBetweenLeftStr:nil andRightStr:@"*"];
         }
         
-        NSArray *ruleArray = [self ruleForClassName:classNameStr andPropertyName:propertyNameStr ruleName:@"" path:@""];
-     
+        for (NSDictionary *dic in dataDic.allValues) {
+           NSMutableDictionary *rules = dic[@"rule"];
+           NSString *dicKey = dic[@"path"];
+           NSArray *ruleArray = [self ruleForClassName:classNameStr andPropertyName:propertyNameStr rules:rules];
+    
+           //通用规则
+           NSMutableArray *propertyArray = [NSMutableArray arrayWithArray:self.propertyDics[dicKey]];
+           if (ruleArray.count>0 && propertyArray) {
+               [propertyArray addObject:ruleArray];
+               self.propertyDics[dicKey] = [propertyArray copy];
+            }
+        }
         
     }
-//        //懒加载
-//        NSArray *lazyGetArray = [self getterForClassName:classNameStr andPropertyName:propertyNameStr];
-//        if (lazyGetArray.count>0) {
-//            [self.lazyArray addObject:lazyGetArray];
-//        }
-//         //获取布局
-//        NSArray *constraintsArr = [self addConstraintsForClassName:classNameStr PropertyName:propertyNameStr];
-//        if (constraintsArr.count>0) {
-//            [self.containtsArray addObject:constraintsArr];
-//        }
-//        //获取添加subView
-//        NSArray *viewsArr = [self addSubViewForClassName:classNameStr PropertyName:propertyNameStr];
-//        if (viewsArr.count>0) {
-//            [self.subviewsArray addObject:viewsArr];
-//        }
-//        //初始化所有属性
-//        NSArray *propertyArr = [self propertyInitForClassName:classNameStr PropertyName:propertyNameStr];
-//        if (propertyArr.count>0) {
-//            [self.propertyValueArray addObject:propertyArr];
-//        }
-    //替换字符串
-    [self addBufferInsertInvocation:invocation];
-
 }
 //进行判断进行替换
--(void)addBufferInsertInvocation:(XCSourceEditorCommandInvocation *)invocation{
-    for (NSInteger i = 0; i < self.lineCount; i ++) {
+-(void)addBufferInsertInvocation:(XCSourceEditorCommandInvocation *)invocation {
+    NSArray *bufferLines = invocation.buffer.lines;
+    for (NSInteger i = 0; i < [bufferLines count]; i ++) {
         NSString *lineStr = invocation.buffer.lines[i];
-        
-        if ([self checkCurrentString:lineStr isContainsString:kGetterFormater]) {
-            [self addCheckLineCoutWithCurrentIndex:i formaterArray:self.lazyArray];
-            [self addBufferWithCurrentLineIndex:i formaterArray:self.lazyArray invocation:invocation];
-            
-        }else if ([self checkCurrentString:lineStr isContainsString:kMasonryFormater]) {
-            [self addCheckLineCoutWithCurrentIndex:i formaterArray:self.containtsArray];
-            [self addBufferWithCurrentLineIndex:i formaterArray:self.containtsArray invocation:invocation];
-            
-        }else if ([self checkCurrentString:lineStr isContainsString:kAddSubviewFormater]){
-            [self addCheckLineCoutWithCurrentIndex:i formaterArray:self.subviewsArray];
-            [self addBufferWithCurrentLineIndex:i formaterArray:self.subviewsArray invocation:invocation];
-            
-        }else if ([self checkCurrentString:lineStr isContainsString:kInitFormater] || [self checkCurrentString:lineStr isContainsString:kInitNodeFormater]){
-            [self addCheckLineCoutWithCurrentIndex:i formaterArray:self.propertyValueArray];
-            [self addBufferWithCurrentLineIndex:i formaterArray:self.propertyValueArray invocation:invocation];
-            
+        //按照规则通用情况
+        NSArray *propertyValue = [self checkCurrentString:lineStr isContainsWithPropertyDics:self.propertyDics];
+        if (lineStr && propertyValue.count > 0) {
+            [self addBufferWithCurrentLineIndex:i formaterArray:[propertyValue copy] invocation:invocation];
         }
     }
 }
+
 -(void)addBufferWithCurrentLineIndex:(NSInteger)currentLineIndex formaterArray:(NSMutableArray *)formaterArray  invocation:(XCSourceEditorCommandInvocation *)invocation {
     //这里的循环主要就是开始 在检测到的下一行开始轮询
     for (NSInteger i = currentLineIndex + 1; i < formaterArray.count + currentLineIndex + 1; i ++) {
@@ -128,100 +100,42 @@
         }
     }
 }
--(BOOL)checkCurrentString:(NSString *)lineString isContainsString:(NSString *)isContainsString{
-    if ([lineString containsString:isContainsString] || [[lineString lowercaseString] isEqualToString:isContainsString]){
-        return YES;
+-(NSArray *)checkCurrentString:(NSString *)lineString isContainsWithPropertyDics:(NSDictionary *)isContainsWithPropertyDics {
+    NSString *str = [lineString stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *finalStr = [str stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    if ([isContainsWithPropertyDics objectForKey:finalStr]){
+        return [isContainsWithPropertyDics objectForKey:finalStr];
     }
-    return NO;
+    return nil;
 }
+
+//目前没有使用 废弃
 -(void)addCheckLineCoutWithCurrentIndex:(NSInteger)currentIndex formaterArray:(NSMutableArray *)formaterArray{
     for (NSInteger i = currentIndex + 1; i < formaterArray.count + currentIndex + 1; i ++) {
         NSArray *formatArr = [formaterArray objectAtIndex:formaterArray.count - i - 1  + (currentIndex + 1 )];
         self.lineCount += formatArr.count;
     }
 }
-//rules
-- (NSArray *)ruleForClassName:(NSString *)className andPropertyName:(NSString *)propertyName ruleName:(NSString *)ruleName rules:(NSArray<NSString *> *)rules path:(NSString *)path {
-    NSString *str = @"";
+//根据规则匹配,如包含DisplayNode->生成特定字符串->将propertyName,className替换为
+- (NSArray *)ruleForClassName:(NSString *)className andPropertyName:(NSString *)propertyName rules:(NSMutableDictionary *)rules {
     NSArray *formaterArr = [NSArray array];
-    //轮询规则
-    for (NSString *rule in rules) {
-      if ([className containsString:rule] || [[className lowercaseString] isEqualToString:rule]){
-           str = [path stringByReplacingOccurrencesOfString:@"propertyName" withString:propertyName];
-           str = [str stringByReplacingOccurrencesOfString:@"className" withString:className];
-           [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
+    for (NSDictionary *rule in rules) {
+        NSString *ruleKey = rule.allKeys[0];
+        NSString *ruleValue = rule.allValues[0];
+        if ([className containsString:ruleKey] || [[className lowercaseString] isEqualToString:ruleKey] || [[ruleKey lowercaseString] isEqualToString:@"all"]){
+             NSString *str = [NSString stringWithFormat:@"%@",ruleValue];
+             str = [ruleValue stringByReplacingOccurrencesOfString:@"propertyName" withString:propertyName];
+             str = [str stringByReplacingOccurrencesOfString:@"className" withString:className];
+             formaterArr = [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
         }
     }
     return formaterArr;
 }
-
-
 
 //懒加载
 - (NSArray *)getterForClassName:(NSString *)className andPropertyName:(NSString *)propertyName{
-    NSString *str = @"";
-    if ([className containsString:kUIButton]){
-        str = [NSString stringWithFormat:kASButtonFormater,className,propertyName,propertyName,propertyName,className,
-               propertyName,propertyName,propertyName,propertyName,propertyName,propertyName,propertyName];
-    }else if ([className containsString:kUILabel]){
-        str = [NSString stringWithFormat:kASUILabelFormater,className,propertyName,propertyName,propertyName,className,
-               propertyName,propertyName,propertyName,propertyName,propertyName];
-    }else if ([className containsString:kCommand]){
-        str = [NSString stringWithFormat:kASCommandFormater,propertyName,propertyName,propertyName,propertyName];
-    }else{
-        str = [NSString stringWithFormat:kASCommonFormater,className,propertyName,propertyName,propertyName,className,propertyName];
-    }
-    NSArray *formaterArr = [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
-    return formaterArr;
-}
-//获取布局
-- (NSArray *)addConstraintsForClassName:(NSString *)className PropertyName:(NSString *)propertyName {
-    if ([className containsString:kViewModel]) {
-          return [NSMutableArray array];
-    }else if ([className containsString:kButton] || [className containsString:kView] ||[className containsString:kLabel] || [className containsString:kUIImageView] || [className containsString:kTextField] || [className containsString:kTextView]) {
-        NSString *str = [NSString stringWithFormat:kASMasonryFormater,propertyName];
-        NSArray *conArr = [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
-        return conArr;
-    }
-    return [NSMutableArray array];
-}
- //获取添加subView
-- (NSArray *)addSubViewForClassName:(NSString *)className PropertyName:(NSString *)propertyName {
-    if ([className containsString:kViewModel]) {
-        return [NSMutableArray array];
-    }else if ([className containsString:kButton] || [className containsString:kView] ||[className containsString:kLabel] || [className containsString:kUIImageView] || [className containsString:kTextField] || [className containsString:kTextView]) {
-        NSString *str = [NSString stringWithFormat:kASAddsubviewFormater,propertyName];
-        NSArray *conArr = [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
-        return conArr;
-    }
-    return [NSMutableArray array];
-}
-//初始化所有属性
-- (NSArray *)propertyInitForClassName:(NSString *)className PropertyName:(NSString *)propertyName {
-     NSString *str = @"";
-    if ([className containsString:@"AS"]) {
-        if([className containsString:kTextNode2]){
-            str = [NSString stringWithFormat:kUITextNode2InitFormater,propertyName,propertyName,propertyName,propertyName];
-        }else if([className containsString:kButtonNode]){
-            str = [NSString stringWithFormat:kUIButtonNodeInitFormater,propertyName,propertyName,propertyName,propertyName];
-        }else if([className containsString:kUINetworkImageNode]){
-            str = [NSString stringWithFormat:kUINetworkImageNodeInitFormater,propertyName,propertyName,propertyName];
-        }else if([className containsString:kImageNode]){
-            str = [NSString stringWithFormat:kUIImageNodeInitFormater,propertyName,propertyName,propertyName,propertyName,propertyName];
-        }else if([className containsString:kDisplayNode]){
-            str = [NSString stringWithFormat:kUIDisplayNodeInitFormater,propertyName,propertyName,propertyName];
-        }
-    }else{
-        if ([className containsString:kButton]) {
-            str = [NSString stringWithFormat:kUIButtonInitFormater,propertyName,propertyName];
-        }else if([className containsString:kLabel]){
-            str = [NSString stringWithFormat:kLabelInitFormater,propertyName];
-        }else if([className containsString:kUIImageView]){
-            str = [NSString stringWithFormat:kUIImageViewInitFormater,propertyName];
-        }
-    }
-    NSArray *conArray = [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
-    return conArray;
+    NSString *str = [NSString stringWithFormat:kASCommonFormater,className,propertyName,propertyName,propertyName,className,propertyName];
+    return [[str componentsSeparatedByString:@"\n"] arrayByAddingObject:@""];
 }
 #pragma mark - Get
 -(NSMutableArray *)lazyArray{
@@ -230,24 +144,15 @@
     }
     return _lazyArray;
 }
--(NSMutableArray *)containtsArray{
-    if (_containtsArray == nil) {
-        _containtsArray = [[NSMutableArray alloc] init];
+
+- (NSMutableDictionary *)propertyDics{
+    if (_propertyDics == nil) {
+        _propertyDics = [NSMutableDictionary dictionary];
     }
-    return _containtsArray;
+    return _propertyDics;
 }
--(NSMutableArray *)subviewsArray{
-    if (_subviewsArray == nil) {
-        _subviewsArray = [[NSMutableArray alloc] init];
-    }
-    return _subviewsArray;
-}
-- (NSMutableArray *)propertyValueArray{
-    if (_propertyValueArray == nil) {
-        _propertyValueArray = [[NSMutableArray alloc] init];
-    }
-    return _propertyValueArray;
-}
+
+
 +(ASAutoLayoutViewCode *)sharedInstane{
     static dispatch_once_t predicate;
     static ASAutoLayoutViewCode * sharedInstane;
